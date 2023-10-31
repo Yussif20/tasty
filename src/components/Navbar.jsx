dfd4a18fa269c9de16df2a08ca99a3e30f1e8877
Yousef <yousefayman2000500@gmail.com>
2023-10-31 21:22:26 +0200

    adding the navbar pt.2

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { AiOutlineMenu } from "react-icons/ai";
+
+const Navbar = () => {
+  return (
+    <>
+      <h1>NAvvvvvv</h1>
+      <AiOutlineMenu size={30} />
+    </>
+  );
+};
+
+export default Navbar;
